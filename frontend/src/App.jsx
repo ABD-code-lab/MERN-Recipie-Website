42278fe299e659f93f31867c7dbd7297d3085db6
Ahssan23 <ahssan180sx@gmail.com>
2025-12-01 18:27:27 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,27 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // <-- change here
 import AuthProvider from "./context/AuthContext";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import RecipePage from "./pages/RecipePage";
-import CreateRecipe from "./pages/CreateRecipe";  // <-- Added
+import CreateRecipe from "./pages/CreateRecipe";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter> {/* <-- change here */}
       <AuthProvider>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-
-          {/* Show all recipes */}
-          <Route path="/recipes" element={<RecipePage />} /> 
-
-          {/* Create new recipe page */}
-          <Route path="/recipes/create" element={<CreateRecipe />} /> 
+          <Route path="/recipes" element={<RecipePage />} />
+          <Route path="/recipes/create" element={<CreateRecipe />} />
         </Routes>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
